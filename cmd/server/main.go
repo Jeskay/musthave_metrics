3f52d3da6274772aa7a2a359612986b5cd228ef7
jeskay <hrvp@mail.ru>
2024-10-05 22:25:07 +0300

    address flag added for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"regexp"
+
 	"github.com/Jeskay/musthave_metrics/internal/metric/routes"
 
 	"github.com/Jeskay/musthave_metrics/internal/metric"
 )
 
+var address string = ":8080"
+
 func main() {
 	service := metric.NewMetricService()
 	r := routes.Init(service)
 
-	r.Run(`:8080`)
+	r.Run(address)
+}
+
+func init() {
+	flag.Func("a", "server address", func(s string) error {
+		if len(s) == 0 {
+			return nil
+		}
+		ok, err := regexp.Match(`[^\:]*:[0-9]{4}`, []byte(s))
+		if !ok {
+			return errors.New("invalid address format")
+		}
+		address = s
+		return err
+	})
+	flag.Parse()
 }
