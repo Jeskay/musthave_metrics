b5cd1e0e31dd0e5f89c69a1369941ec4e5441f57
jeskay <hrvp@mail.ru>
2024-11-14 23:17:19 +0300

    remove fatal exit for agent if server does not respond

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -27,7 +27,6 @@ func main() {
 	err := svc.CheckApiAvailability()
 	if err != nil {
 		slog.Error(err.Error())
-		os.Exit(1)
 	}
 	endMonitor = svc.StartMonitoring(conf.GetReportInterval())
 	endSender = svc.StartSending(conf.GetPollInterval())
