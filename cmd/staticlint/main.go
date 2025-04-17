e1fc3c3a1ee12c4b4a62ec7ea436bafa93f2fabb
jeskay <hrvp@mail.ru>
2025-04-17 15:59:10 +0300

    added custom static analysis tool

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,56 @@
+// Package main includes multichecker for static analysis of the source code.
+//
+// Static analysis includes following analyzers:
+//
+//   - Printf
+//
+//   - Shadow
+//
+//   - Shift
+//
+//   - Ineffassign
+//
+//   - Errcheck
+//
+//   - Structtag
+//
+//   - Ossignal
+//
+//   - Staticcheck analyzers
+//
+//     To run static analysis build cmd/staticlint directory and run built binary with provided package directory to analyze.
+//     Use help flag to learn about analyzer-specific options.
+package main
+
+import (
+	"github.com/gordonklaus/ineffassign/pkg/ineffassign"
+	"github.com/kisielk/errcheck/errcheck"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/shift"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"honnef.co/go/tools/staticcheck"
+)
+
+func main() {
+
+	var staticChecks []*analysis.Analyzer = []*analysis.Analyzer{
+		printf.Analyzer,
+		shadow.Analyzer,
+		shift.Analyzer,
+		ineffassign.Analyzer,
+		errcheck.Analyzer,
+		structtag.Analyzer,
+		OsSignalAnalyzer,
+	}
+
+	for _, v := range staticcheck.Analyzers {
+		staticChecks = append(staticChecks, v.Analyzer)
+	}
+
+	multichecker.Main(
+		staticChecks...,
+	)
+}
